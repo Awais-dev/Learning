8d5a309e15f03c4cbd07e826364dac5819f27c5d
Awais <awaishussain@etech.net>
2019-09-03 14:05:07 +0100

    value types

--- MemoryManagement/Program.cs
+++ MemoryManagement/Program.cs
@@ -6,17 +6,36 @@ namespace MemoryManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee e;
+            Int32 year;
+            e = new Manager();
+            e = Employee.Lookup("Joe");
+            year = e.GetYearsEmployed();
+            e.GetProgressReport();
         }
     }
 
-    class Employee
+   internal class Employee
     {
-
+        public Int32  GetYearsEmployed()
+        {
+            return GetYearsEmployed();
+        }
+        public virtual String GetProgressReport()
+        {
+            return GetProgressReport();
+        }
+        public static Employee Lookup(String name)
+        {
+            return Lookup(name);
+        }
     }
 
-    class Manager
+   internal class Manager : Employee
     {
-
+        public override String GetProgressReport()
+        {
+            return GetProgressReport();
+        }
     }
 }
