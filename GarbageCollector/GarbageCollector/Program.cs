e0402fafb0fb3671db6137a5e7f063f6996629ad
Awais <awaishussain@etech.net>
2019-09-04 11:36:50 +0100

    garbage collections

--- GarbageCollector/GarbageCollector/Program.cs
+++ GarbageCollector/GarbageCollector/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
-namespace GarbageCollector
+class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        //Console.WriteLine("Total memory: " + GC.GetTotalMemory(true));
+
+        car car = new car();
+        car.Test();
+        car.make = "Seat";
+
+        //Console.WriteLine("Totalmemoryafterobject: " + GC.GetTotalMemory(true));
+
+        Console.WriteLine($"GC Generation for object: { GC.GetGeneration(car)}");
+
+        Console.WriteLine("Totalmemoryafterobject: " + GC.CollectionCount(0));
+        Console.WriteLine("Totalmemoryafterobject: " + GC.CollectionCount(1));
+        Console.WriteLine("Totalmemoryafterobject: " + GC.CollectionCount(2));
+
+        GC.Collect(0);
+        Console.WriteLine("GC Count after Forcecollect: " + GC.CollectionCount(0));
+    }
+}
+
+class car
+{
+    public string make { get; set; }
+
+    public string Test()
+    {
+        return "Test";
     }
 }
