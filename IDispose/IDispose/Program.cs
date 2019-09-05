36f32018fe72ae10e5ef416958d600ea12489a6e
Awais <Awaishussain@etech.net>
2019-09-05 18:16:48 +0100

    idispose

---
+++ IDispose/IDispose/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IDispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
