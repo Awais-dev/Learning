b7c1f956b8bdb424e466148082db9ffbad8ccfc6
Awais <awaishussain@etech.net>
2019-09-05 14:20:16 +0100

    deconstructor

--- GarbageCollector/GarbageCollector/Program.cs
+++ GarbageCollector/GarbageCollector/Program.cs
@@ -5,7 +5,7 @@ class Program
     {
         //Console.WriteLine("Total memory: " + GC.GetTotalMemory(true));
 
-        car car = new car();
+        Car car = new Car();
         car.Test();
         car.make = "Seat";
 
@@ -13,21 +13,26 @@ class Program
 
         Console.WriteLine($"GC Generation for object: { GC.GetGeneration(car)}");
 
-        Console.WriteLine("Totalmemoryafterobject: " + GC.CollectionCount(0));
-        Console.WriteLine("Totalmemoryafterobject: " + GC.CollectionCount(1));
-        Console.WriteLine("Totalmemoryafterobject: " + GC.CollectionCount(2));
+        Console.WriteLine("Count for Gen 0: " + GC.CollectionCount(0));
+        Console.WriteLine("Count for Gen 1: " + GC.CollectionCount(1));
+        Console.WriteLine("Count for Gen 2: " + GC.CollectionCount(2));
 
         GC.Collect(0);
         Console.WriteLine("GC Count after Forcecollect: " + GC.CollectionCount(0));
     }
 }
 
-class car
+class Car
 {
     public string make { get; set; }
 
     public string Test()
     {
         return "Test";
+    }
+
+    ~ Car()
+    {
+        
     }
 }
