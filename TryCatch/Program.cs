61a17d2b1e8e81bf04f60cd1b4bd736e9b02b248
Awais <Awaishussain@etech.net>
2019-09-09 00:47:05 +0100

    trycatch

---
+++ TryCatch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool input = false;
+            int number;
+            while (!input)
+            {
+                try
+                {
+
+                    Console.WriteLine("Enter a number");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    input = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Error Caught, Please enter a number\n");
+                }
+            }
+
+        }
+    }
+}
