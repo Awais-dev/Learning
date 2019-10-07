cff581e769f293b8945c5e531415e992edb7aabc
Awais <awaishussain@etech.net>
2019-10-07 10:58:58 +0100

    complete the angular practicwe

--- angular-practice/src/app/app.module.ts
+++ angular-practice/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
